11e05cc4f489ba0112e26e4d9140ba7fb3006ac3
frknkmrc7 <39014000+frknkmrc7@users.noreply.github.com>
2020-07-24 00:31:02 +0300

    Add files via upload

---
+++ Uygulama-2 ehliyet_uygulaması.py
@@ -0,0 +1,8 @@
+yas=int(input("UYaşınızı giriniz: "))
+if yas>=18:
+     print("Ehliyet alabilirsin")
+     print("Araban var mı?")
+else:
+     print("Ehliyet alamazsın")
+     print("Büyüde gel")
+     
